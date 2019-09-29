96b7026faa9276ca10aad213ed43c381fbbe390d
Gagandeep Singh <gaganmanku96@gmail.com>
2019-09-29 11:48:28 +0530

    added 2 basic apps

---
+++ 2. Simple Prediction Model/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def print_square():
+    recieved_value = int(request.get_json(force=True))
+    return str(recieved_value**2)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
